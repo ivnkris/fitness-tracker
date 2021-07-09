590a7f1c9dfded6ccedf62ed4395c703da7321ad
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-07-09 19:05:50 +0100

    added notes to server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+// set up express
+
+// connect to mongo.db
+
+// add routes: view - /stats; /exercise; /
+// api: /workouts - / GET all; /:id PUT single workout; / POST workout; /range GET aggregate??
