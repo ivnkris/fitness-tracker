a1b6fe339fc1868ea3afb7f1dd48ebb39ec64af8
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-07-11 10:35:21 +0100

    added express server and mongoDB connection

--- server.js
+++ server.js
@@ -4,3 +4,27 @@
 
 // add routes: view - /stats; /exercise; /
 // api: /workouts - / GET all; /:id PUT single workout; / POST workout; /range GET aggregate??
+
+const express = require("express");
+
+const { connect } = require("./db");
+// const routes = require("./routes");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(routes);
+
+const init = async () => {
+  await connect();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
+
+init();
