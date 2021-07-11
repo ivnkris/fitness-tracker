2c17071e20992518d9b1c565b58fb173df772e2e
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-07-11 10:25:15 +0100

    added Workout model

--- models/Workout.js
+++ models/Workout.js
@@ -1,9 +1,42 @@
-// day: Date
-// exercises: [{
-//   type: Enum
-//   name: string
-//   duration: integer
-//   weight: integer
-//   reps: integer
-//   sets: integer
-// }]
+const mongoose = require("mongoose");
+
+const schema = {
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+};
+
+const WorkoutSchema = new mongoose.Schema(schema);
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
